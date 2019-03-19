762a3377c771b9e95b548d34bde3445c9ce3b0cc
Bearaffairs <Itsabearaffair@gmail.com>
2019-03-19 13:57:16 +1030

    added notes from talk with David about iterative code

--- iterativeQS.c
+++ iterativeQS.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <math.h>
 
 #define SIZE 10
 
@@ -44,14 +45,29 @@ int main(){
 
 
 void quickSort(int *array, int leftBound, int rightBound){ 
-     
-    stackI stack; stackCreate(&stack, 10);
+     /*
+      * Create temp array to hold 1+logN pairs of L+R bounds
+      *     - Push larger first
+      *  
+      *  Define bounds of array creating a subarray and push onto stack
+      *     - avoid immediate pop after a push
+      *     - two bounds to push/pop at a time
+      *     - Remeber FILO
+      *  
+      *  While subarrays to pop 
+      *     - if >2 elements, arrange as follows
+      *         -choose one element as pivot
+      *         - partition array into smaller and larger
+      */
+    stackI stack; 
+    stackCreate(&stack, (int)(log10(sizeof(&array)/sizeof(&array[0]))+1));
     if(leftBound < rightBound){
-           
+               
         int part = partition(array, leftBound, rightBound);
-        quickSort(array, leftBound, part-1); //smaller part
+    /*    quickSort(array, leftBound, part-1); //smaller part
         quickSort(array, part+1, rightBound); //larger part
-   }   
+   
+   */ }   
 }
 
 //Look at implementing pre-increment instead of post increment for faster/efficient code
